03cb0bec523ed065724c3ea1115f946412f7093b
wrya-mhamad <56258840+wrya-mhamad@users.noreply.github.com>
2020-08-06 15:02:12 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const socket = io('https://progrtc.herokuapp.com');
 //$('#div-chat').hide();
 
-const peer = new Peer({ 
+const peer = new Peer(socket.id{ 
     host: 'progrtc.herokuapp.com',
     port: 9000,
     path: '/myapp',
